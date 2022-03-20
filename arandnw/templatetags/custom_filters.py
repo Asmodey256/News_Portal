015c78ccc533af42cafb618eebf5072af9d811b3
Asmodey256 <97547000+Asmodey256@users.noreply.github.com>
2022-03-20 19:58:39 +0300

    Add files via upload

--- arandnw/templatetags/custom_filters.py
+++ arandnw/templatetags/custom_filters.py
@@ -6,6 +6,9 @@ WORDS = ['лесом', 'дали', 'нафнаф', 'big', 'sometitle']
 
 @register.filter(name='Censor')
 def Censor(value):
+    if not isinstance(value, str):
+        raise ValueError('Нельзя цензурировать не строку')
+
     for word in WORDS:
         value = value.replace(word[1:], '*' * (len(word) - 1))
     return value
